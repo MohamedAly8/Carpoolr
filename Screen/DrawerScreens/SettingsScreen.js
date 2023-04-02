9c74f5653a39f55aadd3486665c21319809c71bb
Isra Zahid <zahidi1@mcmaster.ca>
2023-04-02 01:31:06 -0400

    Update styling for settings screen

--- Screen/DrawerScreens/SettingsScreen.js
+++ Screen/DrawerScreens/SettingsScreen.js
@@ -7,7 +7,7 @@ import { useNavigation } from '@react-navigation/native';
 
 
 
-const SettingsScreen = () => {
+  const SettingsScreen = () => {
   const [email, setEmail] = useState(null);
   const [name, setName] = useState(null);
   const [password, setPassword] = useState(null);
@@ -150,13 +150,14 @@ const SettingsScreen = () => {
     <View style={styles.container}>
 {editMode ? (
   <>
+  <View style={styles.settingsLayout}>
     <View style={styles.row}>
       <Text style={styles.label}>Name:</Text>
-      <TextInput style={styles.input} value={newName} onChangeText={setNewName} />
+      <TextInput style={styles.input} value={newName} onChangeText={setNewName} placeholder={name}/>
     </View>
     <View style={styles.row}>
       <Text style={styles.label}>Email:</Text>
-      <TextInput style={styles.input} value={newEmail} onChangeText={setNewEmail} />
+      <TextInput style={styles.input} value={newEmail} onChangeText={setNewEmail} placeholder={email}/>
     </View>
     <View style={styles.row}>
       <Text style={styles.label}>New Password:</Text>
@@ -166,9 +167,11 @@ const SettingsScreen = () => {
       <Text style={styles.label}>Confirm Password:</Text>
       <TextInput style={styles.input} value={confirmPassword} onChangeText={setConfirmPassword} secureTextEntry={true} />
     </View>
+   </View>
   </>
 ) : (
   <>
+  <View style={style=styles.settingsLayout}>
     <View style={styles.row}>
       <Text style={styles.label}>Name:</Text>
       <Text style={styles.value}>{name}</Text>
@@ -191,24 +194,26 @@ const SettingsScreen = () => {
       )}
 
       <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
-        <Text style={styles.button}>{showPassword ? 'Hide Password' : 'Show Password'}</Text>
+        <Text style={styles.showPassword}>{showPassword ? 'Hide Password' : 'Show Password'}</Text>
       </TouchableOpacity>
     </View>
+   </View>
   </>
 )}
-
-<TouchableOpacity style={styles.button} onPress={() => setEditMode(!editMode)}>
-  <Text style={styles.buttonText}>{editMode ? 'Cancel' : 'Edit'}</Text>
-</TouchableOpacity>
-{editMode && (
-  <TouchableOpacity style={styles.button} onPress={handleSaveChanges}>
-    <Text style={styles.buttonText}>Save Changes</Text>
-  </TouchableOpacity>
+<View style={styles.buttonContainer}>
+    <TouchableOpacity style={[styles.editButton, styles.button]} onPress={() => setEditMode(!editMode)}>
+      <Text style={styles.buttonText}>{editMode ? 'Cancel' : 'Edit'}</Text>
+    </TouchableOpacity>
+    {editMode && (
+      <TouchableOpacity style={[styles.saveButton, styles.button]} onPress={handleSaveChanges}>
+        <Text style={styles.buttonText}>Save Changes</Text>
+      </TouchableOpacity>
 )}
-
-<TouchableOpacity style={styles.deleteButton} onPress={handleDeleteAccount}>
+</View>
+<TouchableOpacity style={[styles.deleteButton, styles.button]} onPress={handleDeleteAccount}>
   <Text style={styles.buttonText}>Delete Account</Text>
 </TouchableOpacity>
+
     </View>
   );
 };
@@ -232,21 +237,73 @@ const SettingsScreen = () => {
       marginBottom: 10,
     },
     label: {
-      fontSize: 18,
+      fontSize: 20,
       fontWeight: 'bold',
-      marginRight: 10,
+      marginRight: 20,
       color: '#000',
     },
     value: {
       fontSize: 16,
       color: '#000',
     },
+    showPassword: {
+        fontSize: 16,
+        color: 'blue',
+        textDecorationLine: 'underline',
+        marginLeft: 10,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20,
+        zIndex: -1,
+      },
     button: {
       fontSize: 16,
       color: 'blue',
       textDecorationLine: 'underline',
-      marginLeft: 10,
+      borderRadius: 10,
+      width: '40%',
+      height: 50,
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginBottom: 20,
+      marginRight: 5,
+      marginLeft: 5,
     },
+    editButton: {
+        backgroundColor: '#692ad5'
+    },
+    saveButton: {
+        backgroundColor: '#28a745'
+    },
+    deleteButton: {
+        marginTop: -15,
+        backgroundColor: '#dc3444'
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        width: 150,
+      },
+      settingsLayout: {
+      alignSelf: 'flex-start',
+      marginLeft: 15,
+      marginBottom: 20
+      },
+      value: {
+      fontSize: 18,
+      color: 'black'
+      },
   });
 export default SettingsScreen;
 
